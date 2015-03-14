024350da6172234d6549b19a45d3b9a4747f7c79
BalisticX <ballisticx1@live.com>
2015-03-14 21:15:09 +1300

    Create AFAL_masterfile.h

---
+++ dialogs/AFAL_masterfile.h
@@ -0,0 +1,31 @@
+/*
+	File: AFAL_masterFile.h
+	Author: Ballistic
+	
+	Description:
+  The master list of all dialogs and IDD for After Altis
+*/
+
+#include "AFAL_resource.hpp"
+
+#include "AFAL_map.hpp"
+
+#include "AFAL_vehicleShop.hpp"
+
+#include "AFAL_gunShop.hpp"
+
+#include "AFAL_clothesShop.hpp"
+
+#include "AFAL_itemShop.hpp"
+
+#include "AFAL_playerMenu.hpp"
+
+/*	DIALOG IDD's
+	
+	Map			500
+	playerMenu			1000
+	vehicleShop			2000
+	gunShop				3000
+	clothesShop			4000
+	itemShop			5000
+*/
