a0073bc1cb1bae9f8559fc37db1a502bb8b8c3d9
Ballistic <ballisticx1@live.com>
2015-04-10 22:52:13 +1200

    Update AFAL_masterfile.h

--- dialogs/AFAL_masterfile.h
+++ dialogs/AFAL_masterfile.h
@@ -8,7 +8,9 @@
 
 #include "AFAL_resource.hpp"
 
-#include "AFAL_map.hpp"
+#include "AFAL_playerMenu.hpp"
+
+#include "AFAL_atm.hpp"
 
 #include "AFAL_lockpick.hpp"
 
@@ -20,12 +22,27 @@
 
 #include "AFAL_itemShop.hpp"
 
+#include "AFAL_delivery.hpp"
+
+#include "AFAL_cyber.hpp"
+
+#include "AFAL_policeMenu.hpp"
+
 /*	DIALOG IDD's
-	
-	Intro Title			17
+
+	Title				17
 	Map				500
+	policeMenu			900
 	playerMenu			1000
+		inventory		1100
+		money			1200
+		phone			1300
+		license			1400
+	ATM				1500
 	lockpick			1700
+	delivery			1800
+	cyber				1900
+		xray			1910
 	vehicleShop			2000
 	gunShop				3000
 	clothesShop			4000
